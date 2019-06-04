907318d4b3b9c8d90ad1997e805c1c983b962e02
Martin Meyerhoff <mamhoff@gmail.com>
2019-06-04 13:22:17 +0200

    Add FriendlyShipping::Request
    We need to be agnostic regarding the request/response objects we use,
    and we want to include a log of the requests we send in our response
    objects. This request object includes the three things usually present
    in HTTP requests.

--- lib/friendly_shipping.rb
+++ lib/friendly_shipping.rb
@@ -1,6 +1,7 @@
 require "physical"
 
 require "friendly_shipping/version"
+require "friendly_shipping/request"
 require "friendly_shipping/response"
 require "friendly_shipping/carrier"
 require "friendly_shipping/services/ship_engine"
