804954037b3577f75c3814d56f81a0be1185bdec
Martin Meyerhoff <mamhoff@gmail.com>
2019-10-28 12:31:52 +0100

    Add RateEstimatesOptions for ShipEngine
    This adds a `carrier_ids` array to the plain ShipmentOptions.

--- lib/friendly_shipping/services/ship_engine/rate_estimates_options.rb
+++ lib/friendly_shipping/services/ship_engine/rate_estimates_options.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
+require 'friendly_shipping/shipment_options'
+
 module FriendlyShipping
   module Services
     class ShipEngine
-      class RateEstimatesOptions
+      # options for the rate estimates call
+      #
+      # @attribute carriers [Array<FriendlyShipping::Carrier] a list of the carriers we want to get IDs from.
+      class RateEstimatesOptions < ShipmentOptions
         attr_reader :carriers
 
-        def initialize(carriers: [])
+        def initialize(carriers:, **kwargs)
           @carriers = carriers
+          super kwargs
+        end
+
+        def carrier_ids
+          carriers.map(&:id)
         end
       end
     end
--- spec/friendly_shipping/services/ship_engine/rate_estimates_options_spec.rb
+++ spec/friendly_shipping/services/ship_engine/rate_estimates_options_spec.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe FriendlyShipping::Services::ShipEngine::RateEstimatesOptions do
+  subject { described_class.new(carriers: [double(id: 'se-12345')]) }
+
   it { is_expected.to respond_to(:carriers) }
+  it { is_expected.to be_a(FriendlyShipping::ShipmentOptions) }
 end
