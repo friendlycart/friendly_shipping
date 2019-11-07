193a561c2ce2541deee8eff9729d2f5537781809
Martin Meyerhoff <mamhoff@gmail.com>
2019-11-07 18:45:46 +0100

    Add rate_estimates method for UPS Freight

--- lib/friendly_shipping/services/ups_freight.rb
+++ lib/friendly_shipping/services/ups_freight.rb
@@ -3,6 +3,12 @@
 require 'dry/monads/result'
 require 'friendly_shipping/http_client'
 require 'friendly_shipping/services/ups_freight/shipping_methods'
+require 'friendly_shipping/services/ups_freight/rates_options'
+require 'friendly_shipping/services/ups_freight/rates_package_options'
+require 'friendly_shipping/services/ups_freight/rates_item_options'
+require 'friendly_shipping/services/ups_freight/parse_freight_rate_response'
+require 'friendly_shipping/services/ups_freight/generate_freight_rate_request_hash'
+require 'friendly_shipping/services/ups_freight/generate_ups_security_hash'
 
 module FriendlyShipping
   module Services
@@ -18,6 +24,13 @@ module FriendlyShipping
         shipping_methods: SHIPPING_METHODS
       )
 
+      TEST_URL = 'https://wwwcie.ups.com'
+      LIVE_URL = 'https://onlinetools.ups.com'
+
+      RESOURCES = {
+        rates: '/rest/FreightRate'
+      }.freeze
+
       def initialize(key:, login:, password:, test: true, client: HttpClient.new)
         @key = key
         @login = login
@@ -28,6 +41,34 @@ module FriendlyShipping
 
       def carriers
         Success([CARRIER])
+      end
+
+      # Get rates for a shipment
+      # @param [Physical::Shipment] location The shipment we want to get rates for
+      # @return [Result<ApiResult<Array<Rate>>>] The rates returned from UPS encoded in a
+      #   `FriendlyShipping::ApiResult` object.
+      def rate_estimates(shipment, options:, debug: false)
+        freight_rate_request_hash = GenerateFreightRateRequestHash.call(shipment: shipment, options: options)
+        url = base_url + RESOURCES[:rates]
+        request = FriendlyShipping::Request.new(
+          url: url,
+          body: authentication_hash.merge(freight_rate_request_hash).to_json,
+          debug: debug
+        )
+
+        client.post(request).bind do |response|
+          ParseFreightRateResponse.call(response: response, request: request)
+        end
+      end
+
+      private
+
+      def authentication_hash
+        GenerateUpsSecurityHash.call(key: key, login: login, password: password)
+      end
+
+      def base_url
+        test ? TEST_URL : LIVE_URL
       end
     end
   end
--- spec/friendly_shipping/services/ups_freight_spec.rb
+++ spec/friendly_shipping/services/ups_freight_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'friendly_shipping/services/ups_freight'
 
 RSpec.describe FriendlyShipping::Services::UpsFreight do
-  subject(:service) { described_class.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY']) }
+  let(:service) { described_class.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY']) }
 
   describe 'carriers' do
     subject { service.carriers.value! }
@@ -20,6 +20,134 @@ RSpec.describe FriendlyShipping::Services::UpsFreight do
         'UPS Freight LTL - Guaranteed A.M.',
         'UPS Standard LTL',
       )
+    end
+  end
+
+  describe '#rate_estimates' do
+    let(:shipment) { Physical::Shipment.new(packages: packages, origin: origin, destination: destination) }
+
+    let(:origin) do
+      Physical::Location.new(
+        company_name: 'Developer Test 1',
+        address1: '01 Developer Way',
+        city: 'Richmond',
+        zip: '23224',
+        region: 'VA',
+        country: 'US'
+      )
+    end
+
+    let(:destination) do
+      Physical::Location.new(
+        company_name: 'Consignee Test 1',
+        address1: '000 Consignee Street',
+        city: 'Allanton',
+        zip: '63025',
+        region: 'MO',
+        country: 'US'
+      )
+    end
+
+    let(:packages) { [package_one, package_two] }
+
+    let(:package_one) do
+      Physical::Package.new(
+        id: 'my_package_1',
+        items: [item_one]
+      )
+    end
+
+    let(:package_two) do
+      Physical::Package.new(
+        id: 'my_package_2',
+        items: [item_two]
+      )
+    end
+
+    let(:item_one) do
+      Physical::Item.new(
+        id: 'item_one',
+        weight: Measured::Weight(500, :lbs)
+      )
+    end
+
+    let(:item_two) do
+      Physical::Item.new(
+        id: 'item_two',
+        weight: Measured::Weight(500, :lbs)
+      )
+    end
+
+    let(:options) do
+      FriendlyShipping::Services::UpsFreight::RatesOptions.new(
+        shipping_method: FriendlyShipping::ShippingMethod.new(service_code: '308'),
+        shipper_number: ENV['UPS_SHIPPER_NUMBER'],
+        billing_address: billing_location,
+        customer_context: customer_context,
+        package_options: package_options
+      )
+    end
+
+    let(:customer_context) { 'order-12345' }
+
+    let(:billing_location) do
+      ::Physical::Location.new(
+        name: "Test Testman",
+        company_name: "Acme Co.",
+        address1: "Far away on the outer rim",
+        city: "Durham",
+        zip: "27703",
+        region: "NC",
+        country: "US"
+      )
+    end
+
+    let(:package_options) do
+      [
+        FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
+          package_id: package_one.id,
+          handling_unit: :pallet,
+          item_options: item_one_options
+        ),
+        FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
+          package_id: package_two.id,
+          handling_unit: :pallet,
+          item_options: item_two_options
+        )
+      ]
+    end
+
+    let(:item_one_options) do
+      [
+        FriendlyShipping::Services::UpsFreight::RatesItemOptions.new(
+          item_id: 'item_one',
+          packaging: :carton,
+          freight_class: '92.5',
+          nmfc_code: '16030 sub 1'
+        )
+      ]
+    end
+
+    let(:item_two_options) do
+      [
+        FriendlyShipping::Services::UpsFreight::RatesItemOptions.new(
+          item_id: 'item_two',
+          packaging: :pallet,
+          freight_class: '92.5',
+          nmfc_code: '16030 sub 1'
+        )
+      ]
+    end
+
+    subject { service.rate_estimates(shipment, options: options) }
+
+    it 'has all the right data', vcr: { cassette_name: 'ups_freight/labels/success' } do
+      rates = subject.value!.data
+      expect(rates.length).to eq(1)
+      rate = rates.first
+      expect(rate).to be_a(FriendlyShipping::Rate)
+      expect(rate.total_amount).to eq(Money.new(75_673, 'USD'))
+      expect(rate.shipping_method.name).to eq('UPS Freight LTL')
     end
   end
 end
