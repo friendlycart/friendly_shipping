de54ac181b8d5ed319b3f6835264ee688de15300
Martin Meyerhoff <mamhoff@gmail.com>
2019-11-27 12:20:10 +0100

    Add Pickup Date to Freight Rate options
    We want to be able to pass the pickup date.

--- lib/friendly_shipping/services/ups_freight/rates_options.rb
+++ lib/friendly_shipping/services/ups_freight/rates_options.rb
@@ -16,6 +16,8 @@ module FriendlyShipping
       #     and an options object to create an Array of commodity fields as per the UPS docs.
       # @attribute [Symbol] billing One of the keys in the `BILLING_CODES` constant. How the shipment
       #     would be billed.
+      # @attribute [Date] pickup_date Date of the Pickup
+      # @attribute [String] pickup_comments Additional pickup instructions
       # @attribute [RatesPackageOptions] package_options Options for each of the packages/pallets in this shipment
       class RatesOptions < FriendlyShipping::ShipmentOptions
         BILLING_CODES = {
@@ -29,6 +31,8 @@ module FriendlyShipping
                     :billing_code,
                     :customer_context,
                     :shipping_method,
+                    :pickup_date,
+                    :pickup_comments,
                     :commodity_information_generator
 
         def initialize(
@@ -37,6 +41,8 @@ module FriendlyShipping
           shipping_method:,
           billing: :prepaid,
           customer_context: nil,
+          pickup_date: nil,
+          pickup_comments: nil,
           commodity_information_generator: GenerateCommodityInformation,
           **kwargs
         )
@@ -45,6 +51,8 @@ module FriendlyShipping
           @shipping_method = shipping_method
           @billing_code = BILLING_CODES.fetch(billing)
           @customer_context = customer_context
+          @pickup_date = pickup_date
+          @pickup_comments = pickup_comments
           @commodity_information_generator = commodity_information_generator
           super kwargs.merge(package_options_class: RatesPackageOptions)
         end
