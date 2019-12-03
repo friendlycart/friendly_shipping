50e400bc506c9436fa184416386bc7ba7b686b03
Martin Meyerhoff <mamhoff@gmail.com>
2019-12-03 22:39:50 +0100

    USPS: Add ParseTimeInTransitResponse
    Wow, what an interface. This parses the XML that UPS returns. The same
    XML file has two slightly different ways of representing a timing,
    depending on whether the timing is expedited or not.

---
+++ spec/friendly_shipping/services/usps/parse_time_in_transit_response_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'friendly_shipping/services/usps/parse_time_in_transit_response'
+
+RSpec.describe FriendlyShipping::Services::Usps::ParseTimeInTransitResponse do
+  let(:response_body) { File.open(File.join(gem_root, 'spec', 'fixtures', 'usps', 'time_in_transit_response.xml')).read }
+  let(:response) { double(body: response_body) }
+  let(:request) { FriendlyShipping::Request.new(url: 'http://www.example.com') }
+
+  subject(:result) do
+    described_class.call(response: response, request: request)
+  end
+
+  it { is_expected.to be_success }
+
+  describe 'success value' do
+    subject { result.value!.data }
+
+    it 'parses response' do
+      expect(subject).to be_a(Enumerable)
+      expect(subject.length).to eq(11)
+      first_rate = subject.first
+      expect(first_rate.shipping_method.name).to eq('Priority Mail Express')
+      expect(first_rate.pickup).to eq(Time.new(2019, 12, 0o3))
+      expect(first_rate.delivery).to eq(Time.new(2019, 12, 0o4))
+      expect(first_rate.properties).to eq(
+        commitment: '1-Day'
+      )
+      last_rate = subject.last
+      expect(last_rate.shipping_method.name).to eq('First-Class Package Service')
+      expect(last_rate.pickup).to eq(Time.new(2019, 12, 0o3))
+      expect(last_rate.delivery).to eq(Time.new(2019, 12, 0o5))
+      expect(last_rate.properties).to eq(
+        commitment: '2 Days',
+        destination_type: :hold_for_pickup
+      )
+    end
+  end
+end
