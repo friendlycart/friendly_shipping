a17ba07e2739f342c660ad0ff2901ed37261233b
Matthew Bass <pelargir@gmail.com>
2020-01-30 16:46:36 -0500

    Add readable body attr to Request class

--- spec/friendly_shipping/request_spec.rb
+++ spec/friendly_shipping/request_spec.rb
@@ -5,12 +5,34 @@ require 'spec_helper'
 RSpec.describe FriendlyShipping::Request do
   let(:url) { 'https://www.example.com/labels' }
   let(:body) { "Hello!" }
+  let(:readable_body) { "World" }
   let(:headers) { { "X-Header" => "Nice" } }
 
-  subject { described_class.new(url: url, body: body, headers: headers) }
+  subject do
+    described_class.new(
+      url: url,
+      body: body,
+      readable_body: readable_body,
+      headers: headers
+    )
+  end
 
   it { is_expected.to respond_to(:url) }
   it { is_expected.to respond_to(:body) }
   it { is_expected.to respond_to(:headers) }
   it { is_expected.to respond_to(:debug) }
+
+  describe '#readable_body' do
+    it 'returns readable body' do
+      expect(subject.readable_body).to eq('World')
+    end
+
+    context 'with blank readable body' do
+      let(:readable_body) { '' }
+
+      it 'returns body' do
+        expect(subject.readable_body).to eq('Hello!')
+      end
+    end
+  end
 end
