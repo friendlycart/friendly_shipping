41e134d1781b8af6fa28e761caceb8aeacbdd611
Martin Meyerhoff <mamhoff@gmail.com>
2020-02-06 14:52:12 +0100

    Extent Location Hash Generator to include Phone numbers

--- spec/friendly_shipping/services/ups_freight/generate_location_hash_spec.rb
+++ spec/friendly_shipping/services/ups_freight/generate_location_hash_spec.rb
@@ -17,18 +17,50 @@ RSpec.describe FriendlyShipping::Services::UpsFreight::GenerateLocationHash do
     )
   end
 
-  subject { JSON.parse(described_class.call(location: location).to_json) }
+  subject { described_class.call(location: location) }
 
   it 'has all the right things' do
-    is_expected.to include(
-      "Name" => 'Developer Test 1',
-      "Address" => {
-        "AddressLine" => '01 Developer Way, North, In the Attic',
-        "City" => 'Richmond',
-        "StateProvinceCode" => 'VA',
-        "PostalCode" => '23224',
-        "CountryCode" => "US"
+    is_expected.to eq(
+      Name: 'Developer Test 1',
+      Address: {
+        AddressLine: '01 Developer Way, North, In the Attic',
+        City: 'Richmond',
+        StateProvinceCode: 'VA',
+        PostalCode: '23224',
+        CountryCode: 'US'
       }
     )
+  end
+
+  context 'with a phone number' do
+    let(:location) do
+      Physical::Location.new(
+        company_name: 'Developer Test 1',
+        address1: '01 Developer Way',
+        address2: 'North',
+        address3: 'In the Attic',
+        phone: '999999999',
+        city: 'Richmond',
+        zip: '23224',
+        region: 'VA',
+        country: 'US'
+      )
+    end
+
+    it 'has all the right things' do
+      is_expected.to eq(
+        Name: 'Developer Test 1',
+        Address: {
+          AddressLine: '01 Developer Way, North, In the Attic',
+          City: 'Richmond',
+          StateProvinceCode: 'VA',
+          PostalCode: '23224',
+          CountryCode: 'US'
+        },
+        Phone: {
+          Number: '999999999'
+        }
+      )
+    end
   end
 end
