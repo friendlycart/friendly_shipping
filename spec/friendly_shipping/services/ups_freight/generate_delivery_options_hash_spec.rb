6fa7d040f49ff20c47cfbe30acb56401e9de4b0d
Martin Meyerhoff <mamhoff@gmail.com>
2020-02-06 12:58:02 +0100

    Add Delivery Options Hash Generator

---
+++ spec/friendly_shipping/services/ups_freight/generate_delivery_options_hash_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'friendly_shipping/services/ups_freight/generate_delivery_options_hash'
+
+RSpec.describe FriendlyShipping::Services::UpsFreight::GenerateDeliveryOptionsHash do
+  subject { described_class.call(delivery_options: delivery_options) }
+
+  let(:delivery_options) { FriendlyShipping::Services::UpsFreight::LabelDeliveryOptions.new }
+
+  it { is_expected.to eq(nil) }
+
+  context 'if lift gate is required' do
+    let(:delivery_options) { FriendlyShipping::Services::UpsFreight::LabelDeliveryOptions.new(lift_gate_required: true) }
+
+    it { is_expected.to eq(DeliveryOptions: { LiftGateRequiredIndicator: "" }) }
+  end
+end
