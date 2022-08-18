a535c8ddefad7e58eaa73c229fa2e5c86824dc29
Matthew Bass <matt@candlescience.com>
2022-08-18 14:39:39 -0400

    Add UPS Freight residential/call delivery options

--- lib/friendly_shipping/services/ups_freight/generate_delivery_options_hash.rb
+++ lib/friendly_shipping/services/ups_freight/generate_delivery_options_hash.rb
@@ -7,8 +7,10 @@ module FriendlyShipping
         def self.call(delivery_options:)
           {
             DeliveryOptions: {
+              CallBeforeDeliveryIndicator: delivery_options.call_before_delivery ? "" : nil,
               HolidayDeliveryIndicator: delivery_options.holiday_delivery ? "" : nil,
               InsideDeliveryIndicator: delivery_options.inside_delivery ? "" : nil,
+              ResidentialDeliveryIndicator: delivery_options.residential_delivery ? "" : nil,
               WeekendDeliveryIndicator: delivery_options.weekend_delivery ? "" : nil,
               LiftGateRequiredIndicator: delivery_options.lift_gate_required ? "" : nil,
               LimitedAccessDeliveryIndicator: delivery_options.limited_access_delivery ? "" : nil
