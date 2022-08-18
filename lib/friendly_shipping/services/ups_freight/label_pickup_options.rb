79b9d6caf7a953eaa6ffb214b84477e7a16ed51a
Matthew Bass <matt@candlescience.com>
2022-08-18 14:43:38 -0400

    Add UPS Freight residential pickup option

--- lib/friendly_shipping/services/ups_freight/label_pickup_options.rb
+++ lib/friendly_shipping/services/ups_freight/label_pickup_options.rb
@@ -6,6 +6,7 @@ module FriendlyShipping
       class LabelPickupOptions
         attr_reader :holiday_pickup,
                     :inside_pickup,
+                    :residential_pickup,
                     :weekend_pickup,
                     :lift_gate_required,
                     :limited_access_pickup
@@ -13,12 +14,14 @@ module FriendlyShipping
         def initialize(
           holiday_pickup: nil,
           inside_pickup: nil,
+          residential_pickup: nil,
           weekend_pickup: nil,
           lift_gate_required: nil,
           limited_access_pickup: nil
         )
           @holiday_pickup = holiday_pickup
           @inside_pickup = inside_pickup
+          @residential_pickup = residential_pickup
           @weekend_pickup = weekend_pickup
           @lift_gate_required = lift_gate_required
           @limited_access_pickup = limited_access_pickup
--- spec/friendly_shipping/services/ups_freight/label_pickup_options_spec.rb
+++ spec/friendly_shipping/services/ups_freight/label_pickup_options_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe FriendlyShipping::Services::UpsFreight::LabelPickupOptions do
 
   it { is_expected.to respond_to(:holiday_pickup) }
   it { is_expected.to respond_to(:inside_pickup) }
+  it { is_expected.to respond_to(:residential_pickup) }
   it { is_expected.to respond_to(:weekend_pickup) }
   it { is_expected.to respond_to(:lift_gate_required) }
   it { is_expected.to respond_to(:limited_access_pickup) }
