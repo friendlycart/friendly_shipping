4e6aeb8e8c1e514dc772733c5460f98b1bb0cc3f
Justin Barber <barber.justin@gmail.com>
2023-01-27 15:04:17 -0500

    FriendlyShipping::Label
    UPS uses USPS for the last mile for SurePost. Label needs to be able show this additional tracking number.

---
+++ spec/friendly_shipping/services/ups/label_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe FriendlyShipping::Services::Ups::Label do
+  it { is_expected.to respond_to(:id) }
+  it { is_expected.to respond_to(:shipment_id) }
+  it { is_expected.to respond_to(:tracking_number) }
+  it { is_expected.to respond_to(:service_code) }
+  it { is_expected.to respond_to(:label_href) }
+  it { is_expected.to respond_to(:data) }
+  it { is_expected.to respond_to(:label_format) }
+  it { is_expected.to respond_to(:shipment_cost) }
+  it { is_expected.to respond_to(:cost) }
+  it { is_expected.to respond_to(:label_data) }
+  it { is_expected.to respond_to(:usps_tracking_number) }
+end
