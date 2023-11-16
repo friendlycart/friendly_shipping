431e07109519cf111a39c31b3dbd80639b5558d2
Matthew Bass <matt@candlescience.com>
2023-11-16 14:28:27 -0500

    Refactor ShipEngine to use `ApiErrorHandler`
    Instead of using the older `BadRequestHandler` we should be using the
    `ApiErrorHandler` with a custom `ApiError` subclass.

---
+++ lib/friendly_shipping/services/ship_engine/api_error.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'friendly_shipping/api_error'
+
+module FriendlyShipping
+  module Services
+    class ShipEngine
+      class ApiError < FriendlyShipping::ApiError
+        # @param [RestClient::Exception] cause
+        def initialize(cause)
+          super cause, parse_message(cause)
+        end
+
+        private
+
+        # @param [RestClient::Exception] error
+        # @return [String, nil]
+        def parse_message(error)
+          return error.message unless error.response && error.http_code == 400
+
+          parsed_body = JSON.parse(error.response.body)
+          messages = parsed_body.fetch('errors')&.map { |e| e.fetch('message') }
+          messages&.join(', ')
+        rescue JSON::ParserError, KeyError => _e
+          nil
+        end
+      end
+    end
+  end
+end
