6d646a47c290dcb6c4d5e3882de29da03a38808f
Matthew Bass <matt@candlescience.com>
2023-11-16 14:29:50 -0500

    Refactor ShipEngine LTL to use `ApiErrorHandler`
    Instead of using the older `BadRequestHandler` we should be using the
    `ApiErrorHandler` with a custom `ApiError` subclass.

---
+++ lib/friendly_shipping/services/ship_engine_ltl/api_error.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'friendly_shipping/services/ship_engine/api_error'
+
+module FriendlyShipping
+  module Services
+    class ShipEngineLTL
+      class ApiError < FriendlyShipping::Services::ShipEngine::ApiError
+        # not implemented
+      end
+    end
+  end
+end
--- lib/friendly_shipping/services/ship_engine_ltl/bad_request.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module FriendlyShipping
-  module Services
-    class ShipEngineLTL
-      class BadRequest < ShipEngine::BadRequest; end
-    end
-  end
-end
