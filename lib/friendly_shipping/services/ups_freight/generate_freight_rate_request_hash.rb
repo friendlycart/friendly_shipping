902c9649efee604f92cab87b1a032d4ae53b5d52
Matthew Bass <matt@candlescience.com>
2023-12-21 11:53:27 -0500

    Extract UPS Freight handling units generator

--- lib/friendly_shipping/services/ups_freight/generate_freight_rate_request_hash.rb
+++ lib/friendly_shipping/services/ups_freight/generate_freight_rate_request_hash.rb
@@ -2,6 +2,7 @@
 
 require 'friendly_shipping/services/ups_freight/generate_location_hash'
 require 'friendly_shipping/services/ups_freight/generate_pickup_request_hash'
+require 'friendly_shipping/services/ups_freight/generate_handling_units_hash'
 
 module FriendlyShipping
   module Services
@@ -22,30 +23,12 @@ module FriendlyShipping
                 Commodity: options.commodity_information_generator.call(shipment: shipment, options: options),
                 TimeInTransitIndicator: 'true',
                 PickupRequest: GeneratePickupRequestHash.call(pickup_request_options: options.pickup_request_options),
-              }.compact.merge(handling_units(shipment, options).reduce(&:merge).to_h)
+              }.compact.
+                merge(GenerateHandlingUnitsHash.call(shipment: shipment, options: options))
             }
           end
 
           private
-
-          def handling_units(shipment, options)
-            all_package_options = shipment.packages.map { |package| options.options_for_package(package) }
-            all_package_options.group_by(&:handling_unit_code).map do |_handling_unit_code, options_group|
-              [options_group.first, options_group.length]
-            end.map { |package_options, quantity| handling_unit_hash(package_options, quantity) }
-          end
-
-          def handling_unit_hash(package_options, quantity)
-            {
-              package_options.handling_unit_tag => {
-                Quantity: quantity.to_s,
-                Type: {
-                  Code: package_options.handling_unit_code,
-                  Description: package_options.handling_unit_description
-                }
-              }
-            }
-          end
 
           def request_options(customer_context)
             return {} unless customer_context
