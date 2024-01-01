bb6dfc95b6ab93e8d932852583336d2d806ff0d2
Matthew Bass <matt@candlescience.com>
2024-01-01 10:35:24 -0500

    Add YARD docs to USPS International classes

--- lib/friendly_shipping/services/usps_international/serialize_rate_request.rb
+++ lib/friendly_shipping/services/usps_international/serialize_rate_request.rb
@@ -11,9 +11,9 @@ module FriendlyShipping
           #   shipment.packages[0].properties[:box_name] Can be :variable or a
           #     flat rate container defined in CONTAINERS.
           # @param [String] login The USPS login code
-          # @param [FriendlyShipping::Services::UspsInternational::RateEstimateOptions] options The options
+          # @param [RateEstimateOptions] options The options
           #   object to use with this request.
-          # @return [Array<FriendlyShipping::Rate>] A set of Rates that this package may be sent with
+          # @return [Array<Rate>] A set of Rates that this package may be sent with
           def call(shipment:, login:, options:)
             xml_builder = Nokogiri::XML::Builder.new do |xml|
               xml.IntlRateV2Request('USERID' => login) do
