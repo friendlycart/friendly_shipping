38e94ec4dc4973c7f15bb2a649a140ba4f7d2790
Matthew Bass <matt@candlescience.com>
2024-01-19 14:15:21 -0500

    Add options class for TForce pickup request
    This class holds various options for the TForce pickup request API.

---
+++ lib/friendly_shipping/services/tforce_freight/pickup_options.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module FriendlyShipping
+  module Services
+    class TForceFreight
+      # Options for creating a TForce Freight pickup request
+      class PickupOptions < FriendlyShipping::ShipmentOptions
+        # Service options for pickups
+        SERVICE_OPTIONS = {
+          inside: "INPU",
+          liftgate: "LIFO",
+          freezable: "PFFF",
+          residential: "RESP",
+          extreme_length: "EXLT",
+          trade_show: "TRPU"
+        }.freeze
+
+        # @return [Time] date/time of pickup
+        attr_reader :pickup_at
+
+        # @return [Range] time window for pickup
+        attr_reader :pickup_time_window
+
+        # @return [Array<String>] shipment pickup service options
+        attr_reader :service_options
+
+        # @return [String] instructions for pickup
+        attr_reader :pickup_instructions
+
+        # @return [String] instructions for handling
+        attr_reader :handling_instructions
+
+        # @return [String] instructions for delivery
+        attr_reader :delivery_instructions
+
+        # @param pickup_at [Time] date/time of pickup (defaults to now)
+        # @param pickup_time_window [Range] time window for pickup (defaults to start/end of today)
+        # @param service_options [Array<String>] shipment pickup service options
+        # @param pickup_instructions [String] instructions for pickup
+        # @param handling_instructions [String] instructions for handling
+        # @param delivery_instructions [String] instructions for delivery
+        # @param kwargs [Hash]
+        # @option kwargs [Enumerable<PackageOptions>] :package_options the options for packages in this shipment
+        # @option kwargs [Class] :package_options_class the class to use for package options when none are provided
+        def initialize(
+          pickup_at: Time.now,
+          pickup_time_window: Time.now.beginning_of_day..Time.now.end_of_day,
+          service_options: [],
+          pickup_instructions: nil,
+          handling_instructions: nil,
+          delivery_instructions: nil,
+          **kwargs
+        )
+          @pickup_at = pickup_at
+          @pickup_time_window = pickup_time_window
+          @service_options = service_options
+          @pickup_instructions = pickup_instructions
+          @handling_instructions = handling_instructions
+          @delivery_instructions = delivery_instructions
+
+          validate_service_options!
+
+          super(**kwargs.reverse_merge(package_options_class: RatesPackageOptions))
+        end
+
+        private
+
+        # Raises an exception if the service options passed into the initializer
+        # don't correspond to valid codes from {SERVICE_OPTIONS}.
+        #
+        # @raise [ArgumentError] invalid service options
+        # @return [nil]
+        def validate_service_options!
+          invalid_options = (service_options - SERVICE_OPTIONS.values)
+          return unless invalid_options.any?
+
+          raise ArgumentError, "Invalid service option(s): #{invalid_options.join(', ')}"
+        end
+      end
+    end
+  end
+end
---
+++ spec/friendly_shipping/services/tforce_freight/pickup_options_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe FriendlyShipping::Services::TForceFreight::PickupOptions do
+  subject(:options) do
+    described_class.new(
+      pickup_at: Time.parse("2023-05-18 12:30:00"),
+      pickup_time_window: Time.parse("2023-05-18 08:00:00")..Time.parse("2023-05-18 16:00:00"),
+      service_options: %w[INPU LIFO],
+      pickup_instructions: "East Dock",
+      handling_instructions: "Handle with care",
+      delivery_instructions: "West Dock"
+    )
+  end
+
+  let(:shipping_method) { FriendlyShipping::ShippingMethod.new }
+
+  it { is_expected.to be_a(FriendlyShipping::ShipmentOptions) }
+
+  [
+    :pickup_at,
+    :pickup_time_window,
+    :service_options,
+    :pickup_instructions,
+    :handling_instructions,
+    :delivery_instructions
+  ].each do |option|
+    it { is_expected.to respond_to(option) }
+  end
+
+  it_behaves_like "overrideable package options class" do
+    let(:default_class) { FriendlyShipping::Services::TForceFreight::RatesPackageOptions }
+    let(:required_attrs) { {} }
+  end
+
+  it "has the right attributes" do
+    expect(options.pickup_at).to eq(Time.parse("2023-05-18 12:30:00"))
+    expect(options.pickup_time_window).to eq(Time.parse("2023-05-18 08:00:00")..Time.parse("2023-05-18 16:00:00"),)
+    expect(options.service_options). to eq(%w[INPU LIFO])
+    expect(options.pickup_instructions).to eq("East Dock")
+    expect(options.handling_instructions).to eq("Handle with care")
+    expect(options.delivery_instructions).to eq("West Dock")
+  end
+
+  describe "package options" do
+    subject(:package_options) { options.options_for_package(package) }
+
+    let(:package) { double(package_id: nil) }
+
+    it { is_expected.to be_a(FriendlyShipping::Services::TForceFreight::RatesPackageOptions) }
+  end
+
+  describe "service option validation" do
+    subject(:options) do
+      described_class.new(
+        service_options: %w[bogus invalid]
+      )
+    end
+
+    it do
+      expect { options }.to raise_error(ArgumentError, "Invalid service option(s): bogus, invalid")
+    end
+  end
+end
