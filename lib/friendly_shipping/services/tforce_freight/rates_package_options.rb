86fa03a792dbe56c0f6beb55cc250112262dde19
Matthew Bass <matt@candlescience.com>
2024-01-29 16:32:38 -0500

    Add handling units to TForce rates package opts
    This adds handling unit attrs to the TForce rates package options
    class, enabling serializers to include the handling unit used for each
    package in rate, pickup, and BOL API requests.

--- lib/friendly_shipping/services/tforce_freight/rates_package_options.rb
+++ lib/friendly_shipping/services/tforce_freight/rates_package_options.rb
@@ -7,7 +7,26 @@ module FriendlyShipping
     class TForceFreight
       # Options for packages/pallets within a TForce Freight shipment
       class RatesPackageOptions < FriendlyShipping::PackageOptions
-        def initialize(**kwargs)
+        HANDLING_UNIT_TYPES = {
+          pallet: { code: "PLT", description: "Pallet", handling_unit_tag: 'One' },
+          skid: { code: "SKD", description: "Skid", handling_unit_tag: 'One' },
+          carboy: { code: "CBY", description: "Carboy", handling_unit_tag: 'One' },
+          totes: { code: "TOT", description: "Totes", handling_unit_tag: 'One' },
+          other: { code: "OTH", description: "Other", handling_unit_tag: 'Two' },
+          loose: { code: "LOO", description: "Loose", handling_unit_tag: 'Two' }
+        }.freeze
+
+        attr_reader :handling_unit_description,
+                    :handling_unit_tag,
+                    :handling_unit_code
+
+        def initialize(
+          handling_unit: :pallet,
+          **kwargs
+        )
+          @handling_unit_code = HANDLING_UNIT_TYPES.fetch(handling_unit).fetch(:code)
+          @handling_unit_description = HANDLING_UNIT_TYPES.fetch(handling_unit).fetch(:description)
+          @handling_unit_tag = "handlingUnit#{HANDLING_UNIT_TYPES.fetch(handling_unit).fetch(:handling_unit_tag)}"
           super(**kwargs.reverse_merge(item_options_class: RatesItemOptions))
         end
       end
