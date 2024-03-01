379dac6d8074cb993755f1b37dd54bc33d444f6d
Matthew Bass <matt@candlescience.com>
2024-03-01 12:05:35 -0500

    Fix ShipEngine address validation serializer
    The API expects an array of hashes, we were just passing a single hash.

--- lib/friendly_shipping/services/ship_engine/serialize_address_validation_request.rb
+++ lib/friendly_shipping/services/ship_engine/serialize_address_validation_request.rb
@@ -6,22 +6,24 @@ module FriendlyShipping
       class SerializeAddressValidationRequest
         class << self
           # @param location [Physical::Location]
-          # @return [Hash]
+          # @return [Array<Hash>]
           def call(location:)
-            {
-              name: location.name,
-              phone: location.phone,
-              email: location.email,
-              company_name: location.company_name,
-              address_line1: location.address1,
-              address_line2: location.address2,
-              address_line3: location.address3,
-              city_locality: location.city,
-              state_province: location.region&.code,
-              postal_code: location.zip,
-              country_code: location.country&.code,
-              address_residential_indicator: residential_indicator(location)
-            }
+            [
+              {
+                name: location.name,
+                phone: location.phone,
+                email: location.email,
+                company_name: location.company_name,
+                address_line1: location.address1,
+                address_line2: location.address2,
+                address_line3: location.address3,
+                city_locality: location.city,
+                state_province: location.region&.code,
+                postal_code: location.zip,
+                country_code: location.country&.code,
+                address_residential_indicator: residential_indicator(location)
+              }
+            ]
           end
 
           private
