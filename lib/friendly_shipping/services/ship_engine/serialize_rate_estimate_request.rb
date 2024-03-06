ddc2f771b01d0c64490a6009240ea33d4309a6f4
Matthew Bass <matt@candlescience.com>
2024-03-06 14:20:38 -0500

    Use class << self in ShipEngine rate estimates
    Making it easier to add another class method in the next commit.

--- lib/friendly_shipping/services/ship_engine/serialize_rate_estimate_request.rb
+++ lib/friendly_shipping/services/ship_engine/serialize_rate_estimate_request.rb
@@ -4,28 +4,30 @@ module FriendlyShipping
   module Services
     class ShipEngine
       class SerializeRateEstimateRequest
-        def self.call(shipment:, options:)
-          {
-            carrier_ids: options.carrier_ids,
-            from_country_code: shipment.origin.country.alpha_2_code,
-            from_postal_code: shipment.origin.zip,
-            to_country_code: shipment.destination.country.alpha_2_code,
-            to_postal_code: shipment.destination.zip,
-            to_city_locality: shipment.destination.city,
-            to_state_province: shipment.destination.region.code,
-            weight: {
-              value: shipment.packages.map { |p| p.weight.convert_to(:pound).value.to_f }.sum.round(2),
-              unit: 'pound'
-            },
-            dimensions: {
-              unit: 'inch',
-              length: shipment.packages.map { |p| p.length.convert_to(:inch).value.to_f }.sum.round(2),
-              width: shipment.packages.map { |p| p.width.convert_to(:inch).value.to_f }.sum.round(2),
-              height: shipment.packages.map { |p| p.height.convert_to(:inch).value.to_f }.sum.round(2)
-            },
-            confirmation: 'none',
-            ship_date: options.ship_date.strftime('%Y-%m-%d'),
-          }.merge(SerializeAddressResidentialIndicator.call(shipment.destination))
+        class << self
+          def call(shipment:, options:)
+            {
+              carrier_ids: options.carrier_ids,
+              from_country_code: shipment.origin.country.alpha_2_code,
+              from_postal_code: shipment.origin.zip,
+              to_country_code: shipment.destination.country.alpha_2_code,
+              to_postal_code: shipment.destination.zip,
+              to_city_locality: shipment.destination.city,
+              to_state_province: shipment.destination.region.code,
+              weight: {
+                value: shipment.packages.map { |p| p.weight.convert_to(:pound).value.to_f }.sum.round(2),
+                unit: 'pound'
+              },
+              dimensions: {
+                unit: 'inch',
+                length: shipment.packages.map { |p| p.length.convert_to(:inch).value.to_f }.sum.round(2),
+                width: shipment.packages.map { |p| p.width.convert_to(:inch).value.to_f }.sum.round(2),
+                height: shipment.packages.map { |p| p.height.convert_to(:inch).value.to_f }.sum.round(2)
+              },
+              confirmation: 'none',
+              ship_date: options.ship_date.strftime('%Y-%m-%d'),
+            }.merge(SerializeAddressResidentialIndicator.call(shipment.destination))
+          end
         end
       end
     end
