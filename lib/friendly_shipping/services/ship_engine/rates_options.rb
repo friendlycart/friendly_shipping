7fa81553556b20b760ef76476aee01043b8dfc9c
Matthew Bass <matt@candlescience.com>
2024-03-12 11:04:24 -0400

    Add `comparison_rate_type` to ShipEngine options
    Setting this value to "retail" ensures we get back retail rates (not
    commercial) from ShipEngine for UPS/USPS rates.

--- lib/friendly_shipping/services/ship_engine/rates_options.rb
+++ lib/friendly_shipping/services/ship_engine/rates_options.rb
@@ -9,25 +9,39 @@ module FriendlyShipping
       #
       # @attribute carriers [Array<FriendlyShipping::Carrier>] a list of the carriers we want to get IDs from.
       # @attribute service_code [String] The service code we want to get rates for.
+      # @attribute ship_date [Time] The date we want to ship on.
+      # @attribute comparison_rate_type [String] Set to "retail" for retail rates (UPS/USPS only).
       class RatesOptions < FriendlyShipping::ShipmentOptions
         attr_reader :carriers,
                     :service_code,
-                    :ship_date
+                    :ship_date,
+                    :comparison_rate_type
 
         def initialize(
           carriers:,
           service_code:,
           ship_date: Time.now,
+          comparison_rate_type: nil,
           **kwargs
         )
           @carriers = carriers
           @service_code = service_code
           @ship_date = ship_date
+          @comparison_rate_type = comparison_rate_type
+          validate_comparison_rate_type!
           super(**kwargs.reverse_merge(package_options_class: RatesPackageOptions))
         end
 
         def carrier_ids
           carriers.map(&:id)
+        end
+
+        private
+
+        def validate_comparison_rate_type!
+          return if comparison_rate_type.nil? || comparison_rate_type == "retail"
+
+          raise ArgumentError, "Invalid comparison rate type: #{comparison_rate_type}"
         end
       end
     end
--- spec/friendly_shipping/services/ship_engine/rates_options_spec.rb
+++ spec/friendly_shipping/services/ship_engine/rates_options_spec.rb
@@ -7,17 +7,32 @@ RSpec.describe FriendlyShipping::Services::ShipEngine::RatesOptions do
     described_class.new(
       carriers: [double(id: "se-12345")],
       service_code: "usps_priority_mail",
-      ship_date: Time.parse("2023-11-16")
+      ship_date: Time.parse("2023-11-16"),
+      comparison_rate_type: comparison_rate_type
     )
   end
+
+  let(:comparison_rate_type) { "retail" }
 
   it { is_expected.to respond_to(:carriers) }
   it { is_expected.to respond_to(:service_code) }
   it { is_expected.to respond_to(:ship_date) }
+  it { is_expected.to respond_to(:comparison_rate_type) }
   it { is_expected.to be_a(FriendlyShipping::ShipmentOptions) }
 
   it_behaves_like "overrideable package options class" do
     let(:default_class) { FriendlyShipping::PackageOptions }
     let(:required_attrs) { { carriers: [], service_code: nil } }
+  end
+
+  describe "comparison rate type validation" do
+    let(:comparison_rate_type) { "bogus" }
+
+    it do
+      expect { options }.to raise_error(
+        ArgumentError,
+        "Invalid comparison rate type: bogus"
+      )
+    end
   end
 end
