517c67f07259be5d5c2ef3fb19971ef6c831d383
Matthew Bass <matt@candlescience.com>
2024-03-21 11:36:44 -0400

    Add structure specs to UPS Freight handling units
    These specs were never written and are necessary to verify the class
    works correctly when provided with structures (pallets).

--- spec/friendly_shipping/services/ups_freight/generate_handling_units_hash_spec.rb
+++ spec/friendly_shipping/services/ups_freight/generate_handling_units_hash_spec.rb
@@ -2,33 +2,32 @@
 
 require 'spec_helper'
 
-# TODO write specs for structures
 RSpec.describe FriendlyShipping::Services::UpsFreight::GenerateHandlingUnitsHash do
   subject { described_class.call(shipment: shipment, options: options) }
 
-  let(:shipment) { Physical::Shipment.new(packages: packages) }
-  let(:packages) { [pallet_1, pallet_2, other] }
+  let(:shipment) { Physical::Shipment.new(structures: pallets) }
+  let(:pallets) { [pallet_1, pallet_2, other] }
 
-  let(:pallet_1) { Physical::Package.new(id: "pallet 1") }
-  let(:pallet_2) { Physical::Package.new(id: "pallet 2") }
-  let(:other) { Physical::Package.new(id: "other") }
+  let(:pallet_1) { Physical::Structure.new(id: "pallet 1") }
+  let(:pallet_2) { Physical::Structure.new(id: "pallet 2") }
+  let(:other) { Physical::Structure.new(id: "other") }
 
   let(:options) do
     FriendlyShipping::Services::UpsFreight::RatesOptions.new(
       shipper_number: "123",
       billing_address: billing_address,
       shipping_method: shipping_method,
-      package_options: [
-        FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
-          package_id: "pallet 1",
+      structure_options: [
+        FriendlyShipping::Services::UpsFreight::RatesStructureOptions.new(
+          structure_id: "pallet 1",
           handling_unit: :pallet
         ),
-        FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
-          package_id: "pallet 2",
+        FriendlyShipping::Services::UpsFreight::RatesStructureOptions.new(
+          structure_id: "pallet 2",
           handling_unit: :pallet
         ),
-        FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
-          package_id: "other",
+        FriendlyShipping::Services::UpsFreight::RatesStructureOptions.new(
+          structure_id: "other",
           handling_unit: :other
         )
       ]
@@ -57,5 +56,62 @@ RSpec.describe FriendlyShipping::Services::UpsFreight::GenerateHandlingUnitsHash
         }
       }
     )
+  end
+
+  describe "deprecated packages behavior" do
+    # TODO: Remove when deprecated packages are removed
+
+    let(:shipment) { Physical::Shipment.new(packages: packages) }
+    let(:packages) { [pallet_1, pallet_2, other] }
+
+    let(:pallet_1) { Physical::Package.new(id: "pallet 1") }
+    let(:pallet_2) { Physical::Package.new(id: "pallet 2") }
+    let(:other) { Physical::Package.new(id: "other") }
+
+    let(:options) do
+      FriendlyShipping::Services::UpsFreight::RatesOptions.new(
+        shipper_number: "123",
+        billing_address: billing_address,
+        shipping_method: shipping_method,
+        package_options: [
+          FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
+            package_id: "pallet 1",
+            handling_unit: :pallet
+          ),
+          FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
+            package_id: "pallet 2",
+            handling_unit: :pallet
+          ),
+          FriendlyShipping::Services::UpsFreight::RatesPackageOptions.new(
+            package_id: "other",
+            handling_unit: :other
+          )
+        ]
+      )
+    end
+
+    let(:billing_address) { FactoryBot.build(:physical_location) }
+    let(:shipping_method) { FriendlyShipping::ShippingMethod.new(service_code: "03") }
+
+    it do
+      is_expected.to eq(
+        {
+          HandlingUnitOne: {
+            Quantity: "2",
+            Type: {
+              Code: "PLT",
+              Description: "Pallet"
+            }
+          },
+          HandlingUnitTwo: {
+            Quantity: "1",
+            Type: {
+              Code: "OTH",
+              Description: "Other"
+            }
+          }
+        }
+      )
+    end
   end
 end
