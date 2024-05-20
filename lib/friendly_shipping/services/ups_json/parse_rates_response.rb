70a6b240a59154f71c44281902a3d9c1a66f70ce
dave <dave@candlescience.com>
2024-05-20 12:40:38 -0700

    Fix UPS json rate details, add specs
    There were a bunch of problems with the extra data being returned with
    the rates response. This is not too surprising given that only the top
    level rate costs were the only thing covered by tests.

--- lib/friendly_shipping/services/ups_json/parse_rates_response.rb
+++ lib/friendly_shipping/services/ups_json/parse_rates_response.rb
@@ -32,13 +32,9 @@ module FriendlyShipping
               days_to_delivery = rated_shipment.dig('GuaranteedDelivery', 'BusinessDaysInTransit').to_i
               total_cost = ParseMoneyHash.call(rated_shipment['TotalCharges'], 'TotalCharges').last
               insurance_price = ParseMoneyHash.call(rated_shipment['ServiceOptionsCharges'], 'ServiceOptionsCharges')&.last
-              negotiated_rate = ParseMoneyHash.call(rated_shipment.dig('NegotiatedRates', 'NetSummaryCharges', 'GrandTotal'), 'GrandTotal')&.last
-              negotiated_charges = ParseMoneyHash.call(rated_shipment.dig('NegotiatedRates', 'ItemizedCharges'), 'ItemizedCharges')&.last
-
-              itemized_charges = Array.wrap(rated_shipment['ItemizedCharges'])
-              itemized_charges = itemized_charges.map do |charge|
-                ParseMoneyHash.call(charge, 'MonetaryValue')
-              end
+              negotiated_rate = ParseMoneyHash.call(rated_shipment.dig('NegotiatedRateCharges', 'TotalCharge'), 'TotalCharge')&.last
+              negotiated_charges = extract_charges(rated_shipment.dig('NegotiatedRateCharges', 'ItemizedCharges'), 'ItemizedCharges')
+              itemized_charges = extract_charges(rated_shipment['ItemizedCharges'], 'ItemizedCharges')
 
               rated_shipment_warnings = Array.wrap(rated_shipment['RatedShipmentAlert'])
               rated_shipment_warnings = rated_shipment_warnings.map { |alert| alert["Description"] }
@@ -74,13 +70,13 @@ module FriendlyShipping
             package_array.map do |rated_package|
               currency_code = rated_package.dig('TotalCharges', 'CurrencyCode')
               {
-                transportation_charges: ParseMoneyHash.call(rated_package['TransportationCharges'], 'TransportationCharges'),
-                base_service_charge: ParseMoneyHash.call(rated_package['BaseServiceCharge'], 'BaseServiceCharge'),
-                service_options_charges: ParseMoneyHash.call(rated_package['ServiceOptionsCharges'], 'ServiceOptionsCharges'),
+                transportation_charges: ParseMoneyHash.call(rated_package['TransportationCharges'], 'TransportationCharges')&.last,
+                base_service_charge: ParseMoneyHash.call(rated_package['BaseServiceCharge'], 'BaseServiceCharge')&.last,
+                service_options_charges: ParseMoneyHash.call(rated_package['ServiceOptionsCharges'], 'ServiceOptionsCharges')&.last,
                 itemized_charges: extract_charges(rated_package['ItemizedCharges'], 'ItemizedCharges'),
-                total_charges: ParseMoneyHash.call(rated_package['TotalCharges'], 'TotalCharges'),
-                negotiated_charges: extract_charges(rated_package.dig('NegotiatedRateCharges', 'ItemizedCharges'), 'ItemizedCharges'),
-                rate_modifiers: extract_modifiers(rated_package['RateModifier'], currency_code: currency_code).inject(:merge),
+                total_charges: ParseMoneyHash.call(rated_package['TotalCharges'], 'TotalCharges')&.last,
+                negotiated_charges: extract_charges(rated_package.dig('NegotiatedCharges', 'ItemizedCharges'), 'ItemizedCharges'),
+                rate_modifiers: extract_modifiers(rated_package['RateModifier'], currency_code: currency_code),
                 weight: BigDecimal(rated_package['Weight']),
                 billing_weight: BigDecimal(rated_package.dig('BillingWeight', 'Weight'))
               }.compact
@@ -88,15 +84,21 @@ module FriendlyShipping
           end
 
           def extract_charges(charges, key_name)
+            non_zero_charges = {}
             Array.wrap(charges).map do |charge|
-              ParseMoneyHash.call(charge, key_name)
-            end.compact
+              parsed_charge = ParseMoneyHash.call(charge, key_name)
+              non_zero_charges[parsed_charge[0]] = parsed_charge[1] if parsed_charge.present?
+            end
+            non_zero_charges
           end
 
           def extract_modifiers(modifiers, currency_code:)
+            rate_modifiers = {}
             Array.wrap(modifiers).map do |modifier|
-              ParseRateModifierHash.call(modifier, currency_code: currency_code)
-            end.compact
+              parsed_modifier = ParseRateModifierHash.call(modifier, currency_code: currency_code)
+              rate_modifiers[parsed_modifier[0]] = parsed_modifier[1] if parsed_modifier.present?
+            end
+            rate_modifiers
           end
         end
       end
