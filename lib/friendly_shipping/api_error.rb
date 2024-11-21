8671479b4f4d773df9257b4bcc098ad6a3bedfbd
Ben Akroyd <ben@candlescience.com>
2024-11-21 12:06:32 -0500

    Update FriendlyShipping::ApiError
    It should now check to make sure there is a value for either
    the cause paramater or the message parameter. YARDdoc updated.

--- lib/friendly_shipping/api_error.rb
+++ lib/friendly_shipping/api_error.rb
@@ -6,11 +6,13 @@ module FriendlyShipping
     # @return [RestClient::Exception] the cause of the error
     attr_reader :cause
 
-    # @param cause [RestClient::Exception] the cause of the error
+    # @param cause [RestClient::Exception, nil] the cause of the error
     # @param message [String] optional descriptive message
     def initialize(cause, message = nil)
+      raise ArgumentError, "Must provide either a cause or a message" if cause.nil? && message.nil?
+
       @cause = cause
-      super(message || cause.message)
+      super(message || cause&.message)
     end
   end
 end
