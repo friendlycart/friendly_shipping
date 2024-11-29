5f553bcf9c58aac3dafaaa653efc579f1b8180a3
Matthew Bass <matt@candlescience.com>
2024-11-29 07:25:42 -0500

    Deprecate the `ApiFailure` class
    It's easy to confuse `ApiFailure` with `ApiError` and `ApiResult`. We
    really only need one class to represent an API result, especially
    since we use the `Dry::Monads::Result` library to indicate if a result
    is success/failure. This commit deprecates `ApiFailure` and moves the
    `#failure` attribute alias into `ApiResult` along with the `#to_s`
    behavior which is to just return `data` as a string.
    
    In a future release we can remove the `ApiFailure` class entirely.

--- lib/friendly_shipping/api_failure.rb
+++ lib/friendly_shipping/api_failure.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 module FriendlyShipping
-  # Wraps an API result from a request that failed.
-  class ApiFailure < ApiResult
-    # @!attribute [r] data
-    #   The API failure (typically an exception)
-    alias_method :failure, :data
-
-    # @return [#to_s] a string representation of the failure
-    def to_s
-      failure.to_s
-    end
-  end
+  # @deprecated Use {ApiResult} instead
+  ApiFailure = ApiResult
 end
